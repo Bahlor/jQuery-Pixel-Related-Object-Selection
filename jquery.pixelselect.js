e3af3aeeb0d39821a8e497abdc167bdf175fe024
Bahlor <christian@ghostgames.net>
2013-01-24 17:38:57 +0100

    Update jquery.pixelselect.js

--- jquery.pixelselect.js
+++ jquery.pixelselect.js
@@ -7,16 +7,8 @@
 
 ;(function ($, window, document, undefined) {
 	"use strict";
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
 
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
-
-    // Create the defaults once
+    // Default
     var pluginName = "pixelselect";
     var defaults = {
         over:		null,
@@ -33,10 +25,6 @@
     // The actual plugin constructor
     function pixelselect(element, options) {
         this.element = element;
-        // jQuery has an extend method which merges the contents of two or
-        // more objects, storing the result in the first object. The first object
-        // is generally empty as we don't want to alter the default options for
-        // future instances of the plugin
 
         this.options 	= 	$.extend({}, defaults, options);
         this._defaults 	= 	defaults;
